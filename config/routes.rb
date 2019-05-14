995e860025af1c82e0593043dfc6d6105ab873cc
Michael Villarreal <mvillarreal0605@gmail.com>
2019-05-13 23:48:07 -0400

    generated controllers, views, and models. And also established relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'students/index'
+  get 'students/show'
+  get 'teachers/index'
+  get 'teachers/show'
+  get 'grades/index'
+  get 'grades/show'
+  get 'schools/grades'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
