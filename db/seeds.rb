d683cba017cf6b5e4d61e7a0609f6dceaddb20f2
Michael Villarreal <mvillarreal0605@gmail.com>
2019-05-22 13:48:16 -0400

    Created the seeds file for populating the data base with students, and teachers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+p 'Creating Staff and Student body of school'
+
+grade_id = 1
+prefixes = ['Mr. ', 'Ms. ', 'Mrs. ']
+
+6.times do
+  3.times do
+    teacher = Teacher.new(
+      name: prefixes.sample + Faker::Name.last_name,
+      grade_id: grade_id
+    )
+    teacher.save!
+    25.times do
+      student = Student.new(
+        name: Faker::Name.first_name + Faker::Name.last_name,
+        teacher_id: teacher.id
+      )
+      student.save!
+    end
+  end
+  grade_id += 1
+end
+
+
+p 'Task completed!'
+
